5b40106733f14106ca6c17cd5303fdc95d27b310
Chad Gerckens <Chad.Gerckens@ouhsd.k12.ca.us>
2019-01-21 14:59:19 -0800

    cleaning

--- src/main/java/org/usfirst/frc/team7327/robot/commands/SwerveDrive.java
+++ src/main/java/org/usfirst/frc/team7327/robot/commands/SwerveDrive.java
@@ -21,21 +21,7 @@ public class SwerveDrive extends Command {
 	
 	protected void execute(){
 		
-		Robot.vision.testGearPixy();
-		
-		if(Robot.oi.getYButton(Player1)) {
-			Robot.AddressWorking();
-		}
-		
-		SmartDashboard.putNumber("NavAngle: ", Robot.NavAngle());
-		SmartDashboard.putNumber("Angular Setpoint", Robot.drivetrain.getSteeringSetpoint());
-		SmartDashboard.putNumber("Angular Error", Robot.drivetrain.getSteeringError());
-		SmartDashboard.putNumber("Angular Position", Robot.drivetrain.getSteeringPosition());
-
-		SmartDashboard.putNumber("NWab", Robot.drivetrain.abeNW.get()); 
-		SmartDashboard.putNumber("NEab", Robot.drivetrain.abeNE.get()); 
-		SmartDashboard.putNumber("SWab", Robot.drivetrain.abeSW.get()); 
-		SmartDashboard.putNumber("SEab", Robot.drivetrain.abeSE.get()); 
+		SmartDashboard.putNumber("NavAngle: ", Robot.NavAngle()); 
 		
 		double degreesL = Math.toDegrees(Math.atan2(Robot.oi.getLeftStickY(Player1),  Robot.oi.getLeftStickX(Player1))) + 90;
 		double magnitudeL = Math.sqrt(Math.pow(Robot.oi.getLeftStickX(Player1), 2) + Math.pow(Robot.oi.getLeftStickY(Player1), 2));
@@ -45,40 +31,18 @@ public class SwerveDrive extends Command {
 		
 		if(magnitudeR > .5) setDegree = 360-degreesR;
 		
-		
 		if(Robot.oi.getStartButton(Player1)) Robot.nav.reset();
-		
-		if(Robot.oi.getAButton(Player1))    { setting = 0; Robot.drivetrain.turning.setOn(false); }
-		if(Robot.oi.getLeftBumper(Player1)) { setting = 0; Robot.drivetrain.turning.setOn(false); }
-		if(Robot.oi.getRightBumper(Player1)){ setting = 1; Robot.drivetrain.turning.setOn(false); }
-		if(Robot.oi.getBButton(Player1))    { setting = 4; Robot.drivetrain.turning.setOn(false); }
-		
-		if(Robot.oi.Dpad(Player1) >= 0 )    { setting = 2; Robot.drivetrain.turning.setOn(true);  }
-		
 		
 		switch(setting) {
 		case 0: //Precision Mode 
-		Robot.drivetrain.setAllDegrees(setDegree+Robot.NavAngle());
-		Robot.drivetrain.setAllSpeed(-Robot.oi.getRightTrigger(Player1)+Robot.oi.getLeftTrigger(Player1));
-		if(magnitudeL > .5) { setting = 5; Robot.drivetrain.turning.setOn(true); }
-		break; 
-		case 1: //Turn Mode
-			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
-			Robot.drivetrain.setAllSpeed(Robot.oi.getLeftStickX(Player1)+Robot.oi.getRightTrigger(Player1)-Robot.oi.getLeftTrigger(Player1));
-			break;
-		case 2: //D-Pad in Use
-			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
-			Robot.drivetrain.turning.setYaw(Robot.oi.Dpad(Player1));
-			if(Robot.oi.Dpad(Player1) == -1) { setting = 0; Robot.drivetrain.turning.setOn(false); }
+			Robot.drivetrain.setAllDegrees(setDegree+Robot.NavAngle());
+			Robot.drivetrain.setAllSpeed(-Robot.oi.getRightTrigger(Player1)+Robot.oi.getLeftTrigger(Player1));
+			if(magnitudeL > .5) { setting = 1; Robot.drivetrain.turning.setOn(true); }
 			break; 
-		case 4: //Power Mode
-		Robot.drivetrain.setAllDegrees(setDegree+Robot.NavAngle());
-		Robot.drivetrain.setAllSpeed(Robot.oi.getLeftStickY(Player1)-Robot.oi.getRightTrigger(Player1)+Robot.oi.getLeftTrigger(Player1));
-		break;
-		case 5: 
+		case 1: 
 			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
 			Robot.drivetrain.turning.setYaw(degreesL);
-			if(magnitudeL <= .5) { setting = 4; Robot.drivetrain.turning.setOn(false); }
+			if(magnitudeL <= .5) { setting = 0; Robot.drivetrain.turning.setOn(false); }
 			break; 
 		}
 		
