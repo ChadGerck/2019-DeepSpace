0055cb9f05ebb5f471794075a736939d8086900b
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-02-20 20:26:27 -0800

    SidetoSide
    Need to Test this code, before moving on.

--- src/main/java/org/usfirst/frc/team7327/robot/commands/SwerveDrive.java
+++ src/main/java/org/usfirst/frc/team7327/robot/commands/SwerveDrive.java
@@ -157,19 +157,29 @@ public class SwerveDrive extends Command {
 			Robot.drivetrain.setAllDegrees(setDegree+Robot.NavAngle());
 			Robot.drivetrain.setAllSpeed(-Robot.oi.getRightTrigger(Player1)+Robot.oi.getLeftTrigger(Player1));
 			if(magnitudeR > .5) { setting = 1; Robot.drivetrain.turning.setOn(true); }
-			if(Robot.oi.getLSClick(Player1)){
-				setting = 2; 
-			}
+			if(Robot.oi.getLSClick(Player1)){ setting = 2; }
+			if(Robot.oi.getRSClick(Player1)){ setting = 3; }
 			break; 
 		case 1: 
 			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
 			Robot.drivetrain.turning.setYaw(degreesR);
-			if(magnitudeR <= .5) { setting = 0; Robot.drivetrain.turning.setOn(false); }
+			if(magnitudeR <= .5)            { setting = 0; Robot.drivetrain.turning.setOn(false); }
+			if(Robot.oi.getLSClick(Player1)){ setting = 2; Robot.drivetrain.turning.setOn(false); }
+			if(Robot.oi.getRSClick(Player1)){ setting = 3; Robot.drivetrain.turning.setOn(false); }
 			break; 
 		case 2: 
 			Robot.drivetrain.setAllDegrees(Robot.NavAngle());
 			Robot.drivetrain.setAllSpeed(-Robot.oi.getRightTrigger(Player1)+Robot.oi.getLeftTrigger(Player1));
-			if(magnitudeR > .5) { setting = 1; }
+			if(magnitudeL > .5)             { setting = 0; }
+			if(magnitudeR > .5)             { setting = 1; Robot.drivetrain.turning.setOn(true); }
+			if(Robot.oi.getRSClick(Player1)){ setting = 3; }
+			break;
+		case 3: 
+			Robot.drivetrain.setAllDegrees(Robot.NavAngle());
+			Robot.drivetrain.setAllSpeed(-Robot.oi.getRightTrigger(Player1)+Robot.oi.getLeftTrigger(Player1));
+			if(magnitudeL > .5)             { setting = 0; }
+			if(magnitudeR > .5)             { setting = 1; Robot.drivetrain.turning.setOn(true); }
+			if(Robot.oi.getLSClick(Player1)){ setting = 2; }
 			break;
 		}
 	}
