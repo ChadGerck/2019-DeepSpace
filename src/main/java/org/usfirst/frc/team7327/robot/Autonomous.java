d048a5eb82db9bae9109f0b9bde2048ba27ec9ca
OxnardCodeTeam <47262396+OxnardCodeTeam@users.noreply.github.com>
2019-05-22 16:05:27 -0700

    Update Autonomous.java
    straight

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,7 +5,17 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
     public static void Auto() {
         Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
         Robot.TurnRight();
+       
     }
 }
 
