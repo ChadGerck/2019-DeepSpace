79019b665e16db2ef7ecce1f3491c9f4d736780a
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2019-05-22 16:09:13 -0700

    Sara Code

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -1,11 +1,24 @@
 package org.usfirst.frc.team7327.robot;
 
 import org.usfirst.frc.team7327.robot.Robot; 
-
+//.1 seconds per move forward
 public class Autonomous {
     public static void Auto() {
+        //Robot.MoveForward();
+        //Robot.TurnRight();  
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.MoveForward(); 
+        Robot.TurnRight(); 
         Robot.MoveForward();
-        Robot.TurnRight();
     }
 }
 
