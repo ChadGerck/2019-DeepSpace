befc6c805658c97ec93fec4a2cd2d3464090947d
OxnardCodeTeam <47262396+OxnardCodeTeam@users.noreply.github.com>
2019-05-29 17:57:33 -0700

    5/29/19
    while,if,method

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -56,6 +56,7 @@ public class Autonomous {
         Robot.MoveForward();
         //50 move forwards
         Robot.TurnRight();
+        //1 turn right
         Robot.MoveForward();
         Robot.MoveForward();
         Robot.MoveForward();
@@ -83,7 +84,84 @@ public class Autonomous {
         Robot.MoveForward();        
         Robot.MoveForward();
         //25 move forwards
+        
+        //backwards
+        Robot.MoveForward();
+        Robot.TurnRight();
+        Robot.TurnRight();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+
+        //chad ex
+        int k = 0; 
+        while(k < 10){
+            Robot.MoveForward();
+            k++; 
+        }
+
+        if(k == 10){
+            Robot.MoveForward();
+        }
+
+        //test with whiles
+        
+        int b = 0;
+        while(b < 100){
+            Robot.MoveForward();
+            b++;
+                  
+            if(b == 25){
+            Robot.TurnRight();
+            }
+
+            if(b == 50){
+            Robot.TurnRight();
+            }
+
+            if(b == 75){
+            Robot.TurnRight();
+            }
+
+            if(b == 100){
+            Robot.TurnRight();
+            }
+
+
+            MoveForward(50);
+            TurnRight(1);
+            MoveForward(25);
+        }
+
+    }
+
+    public static void MoveForward(double amount){
+        //of the amount 
+        int s = 0; 
+        while(s < amount){
+            Robot.MoveForward();
+            s++;
+        }
+    }
+        
+    public static void TurnRight(double amount){
+        int s = 0;
+        if(s == amount){
+            Robot.TurnRight();
+        }
+    }
+
+
+
+    }
+
+
+
+
+
 
     }
 }
-
