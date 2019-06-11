7ef4343ae02ea9816456865d0b7a74f08974a8bb
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-06-11 00:31:37 -0700

    cleaning even more code

--- src/main/java/org/usfirst/frc/team7327/robot/OI.java
+++ src/main/java/org/usfirst/frc/team7327/robot/OI.java
@@ -7,14 +7,15 @@ public class OI{
     private static final double DEADZONE_LIMIT = 0.2;
     public final XboxController Controller0 = new XboxController(0), Controller1 = new XboxController(1);
 
-    public OI(){}
-    public double getLeftMagnitude(XboxController Controller){ return Math.hypot(Controller.getRawAxis(1), Controller.getRawAxis(0)); }
-    public double getRightMagnitude(XboxController Controller){ return Math.hypot(Controller.getRawAxis(4), Controller.getRawAxis(5)); }
+    public double RightArc(XboxController Controller){ return Math.toDegrees(Math.atan2(RightY(Controller), RightX(Controller))) + 90; }
+    public double getLeftJoystickAngle(XboxController Controller){ return Math.toDegrees(Math.atan2(Controller.getRawAxis(0), -Controller0.getRawAxis(1))); }
+    public double LeftMag (XboxController Controller){ return Math.hypot(Controller.getRawAxis(1), Controller.getRawAxis(0)); }
+    public double RightMag(XboxController Controller){ return Math.hypot(Controller.getRawAxis(4), Controller.getRawAxis(5)); }
 
-    public double LeftStickX  (XboxController Controller){ double raw = Controller.getRawAxis(0); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
-    public double LeftStickY  (XboxController Controller){ double raw = Controller.getRawAxis(1); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
-    public double RightStickX (XboxController Controller){ double raw = Controller.getRawAxis(4); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
-    public double RightStickY (XboxController Controller){ double raw = Controller.getRawAxis(5); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
+    public double LeftX  (XboxController Controller){ double raw = Controller.getRawAxis(0); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
+    public double LeftY  (XboxController Controller){ double raw = Controller.getRawAxis(1); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
+    public double RightX (XboxController Controller){ double raw = Controller.getRawAxis(4); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
+    public double RightY (XboxController Controller){ double raw = Controller.getRawAxis(5); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
     public double LeftTrigger (XboxController Controller){ double raw = Controller.getRawAxis(2); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
     public double RightTrigger(XboxController Controller){ double raw = Controller.getRawAxis(3); return Math.abs(raw) < DEADZONE_LIMIT ? 0.0 : raw; }
     
@@ -36,24 +37,13 @@ public class OI{
     public boolean RSClick        (XboxController Controller){ return Controller.getStickButtonPressed(Hand.kRight); }
     public double  Dpad           (XboxController Controller){ return Controller.getPOV(); }
     
-    public boolean DpadUp(XboxController Controller){
-        double POV = Controller.getPOV(); 
-        if((POV >= 0 && POV < 45) || (POV >= 315 && POV < 360)) { return true; }
-        else { return false; }
-    }
-    public boolean DpadRight(XboxController Controller){
-        double POV = Controller.getPOV(); 
-        if(POV >= 45 && POV < 135) { return true; }
-        else { return false; }
-    }
-    public boolean DpadDown(XboxController Controller){
-        double POV = Controller.getPOV(); 
-        if(POV >= 135 && POV < 225) { return true; }
-        else { return false; }
-    }
-    public boolean DpadLeft(XboxController Controller){
-        double POV = Controller.getPOV(); 
-        if(POV >= 225 && POV < 315) { return true; }
-        else { return false; }
+    public boolean DpadUp(XboxController Controller){ double POV = Controller.getPOV(); 
+        if((POV >= 0 && POV < 45) || (POV >= 315 && POV < 360)) { return true; } else { return false; }
+    } public boolean DpadRight(XboxController Controller){ double POV = Controller.getPOV(); 
+        if(POV >= 45 && POV < 135) { return true; } else { return false; }
+    } public boolean DpadDown(XboxController Controller){ double POV = Controller.getPOV(); 
+        if(POV >= 135 && POV < 225) { return true; } else { return false; }
+    } public boolean DpadLeft(XboxController Controller){ double POV = Controller.getPOV(); 
+        if(POV >= 225 && POV < 315) { return true; } else { return false; }
     }
 }
--- src/main/java/org/usfirst/frc/team7327/robot/commands/Drive.java
+++ src/main/java/org/usfirst/frc/team7327/robot/commands/Drive.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc.team7327.robot.commands;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team7327.robot.Robot;
@@ -17,57 +15,40 @@ public class Drive extends Command {
   @Override protected void initialize() { }
 
   public static XboxController P1 = oi.Controller0, P2 = oi.Controller1;  
-  double finalAngle, Redthrottle, ballThrottle, rotMag, rightArc, leftX, leftY, rightX, rightY; 
-  double leftMag, rightMag, directMag, diffError, steering_adjust, x, heading_error; 
+  double finalAngle, Redthrottle, ballThrottle, rotMag, rightArc; 
+  double directMag, steering_adjust, x; 
   boolean fixRotation;   
-  double SteerP = -0.025, SteerD = 0.4, SteerP2 = -0.05, SteerD2 = 0.6, lastError = 0, lastError2 = 0;
-  final double DRIVE_K = 0.26, DESIRED_TARGET_AREA = 13.0;
+  double SteerP = -0.05, SteerD = 0.6, lastError = 0;
 
   @Override
   protected void execute() {
-    x = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
-
     if(Robot.oi.AButtonDown(P1)){
-      diffError = lastError2 - heading_error; 
-			steering_adjust = SteerP2*heading_error + SteerD2*diffError;
-      lastError2 = heading_error; 
+      x = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+			steering_adjust = SteerP*-x + SteerD*(lastError+x);
+      lastError = -x; 
     }
-
-    leftX  = oi.LeftStickX(P1);   leftY = oi.LeftStickY(P1);
-    rightX = oi.RightStickX(P1); rightY = oi.RightStickY(P1);
-    leftMag = oi.getLeftMagnitude(P1); rightMag = oi.getRightMagnitude(P1); 
-    if(rightMag > .7){ rightArc = Math.toDegrees(Math.atan2(rightY, rightX)) + 90;
-      try { Robot.kDrivetrain.turning.setYaw(rightArc - Robot.NavAngle());} catch (Exception e) {}
+    if(oi.RightMag(P1) > .7){ try { Robot.kDrivetrain.turning.setYaw(oi.RightArc(P1) - Robot.NavAngle());} catch (Exception e) {}
       rotMag = Robot.kDrivetrain.turning.getPIDOutput();
-    }
-    else{ rotMag = 0; }
+    } else{ rotMag = 0; }
 
-    if( oi.AButtonDown(P1)){ finalAngle = Math.atan2(steering_adjust,leftX); directMag = steering_adjust + leftX; }
-    else if(leftMag >= .3){ finalAngle = Math.toDegrees(Math.atan2(leftX, leftY)) + Robot.NavAngle(); directMag = leftMag; }
-    else if(oi.RightTrigger(P1) > .1) {finalAngle = 0; directMag = -.5*oi.RightTrigger(P1); }
-    else if(oi.LeftTrigger(P1) > .1) {finalAngle = 180; directMag = -oi.LeftTrigger(P1); }
-    else if(oi.RightBumperDown(P1)) { finalAngle = 90; directMag = .5; }
-    else if(oi.LeftBumperDown(P1)) { finalAngle = 270; directMag = .5; }
+    if( oi.AButtonDown(P1)){ finalAngle = Math.atan2(steering_adjust,oi.LeftX(P1)); directMag = steering_adjust + oi.LeftX(P1); }
+    else if(oi.LeftMag(P1) >= .3){ finalAngle = Math.toDegrees(Math.atan2(oi.LeftX(P1), oi.LeftY(P1))) + Robot.NavAngle(); directMag = oi.LeftMag(P1); }
+    else if(oi.RightTrigger(P1) > .1) {finalAngle = 0; directMag = -.5*oi.RightTrigger(P1); } else if(oi.LeftTrigger(P1) > .1) {finalAngle = 180; directMag = -oi.LeftTrigger(P1); }
+    else if(oi.RightBumperDown(P1)) { finalAngle = 90; directMag = .5; } else if(oi.LeftBumperDown(P1)) { finalAngle = 270; directMag = .5; }
     else { directMag = 0; }
 
-    if(oi.LeftBumperDown(P1) || oi.RightBumperDown(P1) || oi.RightTrigger(P1) > .1 || oi.LeftTrigger(P1) > .1 || leftMag > 0.3 || rightMag > 0.2 || oi.AButtonDown(P1) || oi.BButtonDown(P1) || oi.YButtonDown(P1) || oi.XButtonDown(P1)) {
-      fixRotation = true; }
-    else{fixRotation = false;}
-
+    if(oi.LeftBumperDown(P1) || oi.RightBumperDown(P1) || oi.RightTrigger(P1) > .1 || oi.LeftTrigger(P1) > .1 || oi.LeftMag(P1) > 0.3 || oi.RightMag(P1) > 0.2 || oi.AButtonDown(P1) || oi.BButtonDown(P1) || oi.YButtonDown(P1) || oi.XButtonDown(P1)) {
+      fixRotation = true; 
+    } else{fixRotation = false;}
     SwerveMath.ComputeSwerve(finalAngle, directMag, rotMag, fixRotation); 
 
-		if(oi.StartButton(P1)) { Robot.nav.reset(); }
-		if(oi.StartButton(P2)) { Robot.kDrivetrain.ResetElevator(); }
+		if(oi.StartButton(P1)) { Robot.nav.reset(); } if(oi.StartButton(P2)) { Robot.kDrivetrain.ResetElevator(); }
 		
-		if(oi.RightBumperDown(P2)) { Redthrottle = -.6; }
-		else if(oi.LeftBumperDown(P2)) { Redthrottle = .6;}
-		else { Redthrottle = 0; }
-		Robot.kDrivetrain.setRawIntake(Redthrottle);
+		if(oi.RightBumperDown(P2)) { Redthrottle = -.6; } else if(oi.LeftBumperDown(P2)) { Redthrottle = .6;}
+		else { Redthrottle = 0; } Robot.kDrivetrain.setRawIntake(Redthrottle);
 
-		if(oi.getRightMagnitude(P2) > .3) { ballThrottle = .75*oi.RightStickY(P2); }
-		else if(oi.RightBumperDown(P2)) { ballThrottle = .5; }
-		else{ ballThrottle = 0; }
-    Robot.kDrivetrain.setRawBallIn(ballThrottle); 
+		if(oi.RightMag(P2) > .3) { ballThrottle = .75*oi.RightY(P2); } else if(oi.RightBumperDown(P2)) { ballThrottle = .5; }
+		else{ ballThrottle = 0; } Robot.kDrivetrain.setRawBallIn(ballThrottle); 
     
     ElevatorPositions.MoveElevators();
   }
