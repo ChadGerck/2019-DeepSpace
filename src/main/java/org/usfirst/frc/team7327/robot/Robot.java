ccd9858d95af662f5110c39b8020ce30274ba37e
OxnardCodeTeam <47262396+OxnardCodeTeam@users.noreply.github.com>
2019-06-12 16:46:39 -0700

    compressor

--- src/main/java/org/usfirst/frc/team7327/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Robot.java
@@ -3,12 +3,15 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import com.kauailabs.navx.frc.AHRS;
 import org.usfirst.frc.team7327.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.I2C;
 
 public class Robot extends TimedRobot {
   public static final DriveTrain kDrivetrain = new DriveTrain();
   public static final OI oi = new OI();
   public static AHRS nav; 
+  Compressor c0 = new Compressor(0);
   @Override public void robotInit() { nav = new AHRS(I2C.Port.kMXP); }
   @Override public void robotPeriodic() { kDrivetrain.updateDashboard();}
   @Override public void teleopInit() { //kDrivetrain.SetElevatorStatus(); kDrivetrain.ConfigElevator(); 
