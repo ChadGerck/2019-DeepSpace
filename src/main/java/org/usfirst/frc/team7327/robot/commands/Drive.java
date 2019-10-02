e20645319dd2247bf82c14b3eaa913ab49fe6d77
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-10-02 13:48:10 -0700

    Ready

--- src/main/java/org/usfirst/frc/team7327/robot/commands/Drive.java
+++ src/main/java/org/usfirst/frc/team7327/robot/commands/Drive.java
@@ -16,22 +16,16 @@ public class Drive extends Command {
   public Drive() { requires(Robot.kDrivetrain); }
   @Override protected void initialize() { }
   public static XboxController P1 = oi.Controller0, P2 = oi.Controller1;  
-  double finalAngle, Redthrottle, ballThrottle, rotMag, rightArc; 
-  double directMag, steering_adjust, x; 
-  boolean fixRotation;   
-  double SteerP = -0.025, SteerD = 0, lastError = 0;
-  boolean rocketAngle = true; 
-
-  private ShuffleboardTab tab = Shuffleboard.getTab("RocketAngle");
-  private NetworkTableEntry angleR = tab.add("RocketAngle", rocketAngle).getEntry();
-    
-
-  DoubleSolenoid.Value Pincher, Extendor, pullout = Value.kOff; 
 
   @Override protected void execute() {
 
     
-		if(oi.StartButton(P1)) { Robot.nav.reset(); }
+    if(oi.StartButton(P1)) { Robot.nav.reset(); }
+
+    if(oi.LeftX(P1) > .3){
+      Robot.kDrivetrain.setDrive(.2);
+    }
+    
 		
   }
   @Override protected boolean isFinished() { return false;}
--- src/main/java/org/usfirst/frc/team7327/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team7327/robot/subsystems/DriveTrain.java
@@ -1,15 +1,20 @@
 package org.usfirst.frc.team7327.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import org.usfirst.frc.team7327.robot.commands.Drive;
 
 public class DriveTrain extends Subsystem {
   public static TalonSRX drive; 
   public DriveTrain(){
-    drive.set(ControlMode.PercentOutput, 1);
+    drive = new TalonSRX(10); 
   }
   @Override public void initDefaultCommand() { setDefaultCommand(new Drive()); }
+  public void setDrive(double speed){
+    drive.set(ControlMode.PercentOutput, speed);
+  }
   
   public void updateDashboard(){}
 }
