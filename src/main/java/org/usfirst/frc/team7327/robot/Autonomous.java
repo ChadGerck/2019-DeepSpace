f774f2471096c744db9b941a24a9e114a9f17d4d
OxnardCodeTeam <47262396+OxnardCodeTeam@users.noreply.github.com>
2019-10-28 16:55:57 -0700

    saved

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,24 +4,117 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
     public static void Auto() {
-
+/*
         int x = 0; 
-        while(x < 80){
+        while( x < 10){
             Robot.MoveForward();
-            if(x == 30){
-                Robot.TurnRight();
-            }
-            if(x == 40){ 
-                Robot.TurnRight();
-            }
-            if(x == 70){
-                Robot.TurnRight();
-            }
-            if(x == 79){
-                Robot.TurnRight();
-            }
             x = x + 1; 
         }
+     
+
+        if(x == 10){
+            Robot.MoveForward();
+
+        }
+*/
+        int x = 0;
+        while( x < 64){
+            Robot.MoveForward();
+            x = x + 1; 
+            if(x == 30){
+            Robot.TurnRight();
+            Robot.TurnRight();
+            }
+        }
+
+
+
+        
+
+
+        int x = 0;
+        while( x < 30){
+            Robot.MoveForward();
+            x = x + 1;
+        }
+        Robot.TurnRight();
+        Robot.TurnRight();
+        x = 0;
+        while( x < 30){
+            Robot.MoveForward();
+            x = x + 1;
+        }
+        Robot.TurnRight();
+        Robot.TurnRight();
+        
+        /*
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.TurnRight();
+        Robot.TurnRight();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.TurnRight();
+        Robot.TurnRight();
+        */
         
     }
 }
