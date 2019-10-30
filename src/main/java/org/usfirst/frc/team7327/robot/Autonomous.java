e489ecda26eb1ab5bedddb8c74c8c506533d3f69
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-10-29 17:23:01 -0700

    learning while loops

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,8 +4,85 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
     public static void Auto() {
+        
+        int x = 0;
+        while( x < 20){
+            Robot.MoveForward();
+            x++; 
+        } 
+        /*
+
+        if(x == 10){
+            Robot.TurnRight(); 
+        }
+
+        
+
+        int x = 0;
+        while( x < 15){
+            Robot.MoveForward();
+            x = x + 1;
+        }Robot.TurnRight();
+        x = 0;
+        while( x < 5){
+            Robot.MoveForward();
+            x = x + 1;
+        }
+        */
+
+
+        int x = 0;
+        while( x < 20){
+            if(x == 15){
+                Robot.TurnRight();
+            }
+            x = x + 1;
+        }if(x == 5){
+            Robot.MoveForward();
+        }
+        
+
+
+
+
+
+
+        if(x == 15 ){
+            Robot.TurnRight();
+            
+        }
+            
+
+
+
+
+
+
+
+        /*
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
         Robot.MoveForward();
         Robot.TurnRight();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        Robot.MoveForward();
+        */
+
     }
 }
 
