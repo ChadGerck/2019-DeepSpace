fe6310cc23fa9b5ab5153d6c0767ff930aa44873
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-12-12 10:03:44 -0800

    update

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,69 +4,45 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
     public static void Auto() {
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
+        int x = 0; 
+        while(x < 210){
+            Robot.MoveForward();
+            x = x+1; 
+        if(x == 210){
+            Robot.TurnRight();
+        if(x == 100){
+            Robot.TurnRight();
+        if(x == 210){
+                Robot.TurnRight();
+        if(x == 100){
+                Robot.TurnRight();
+        }
+
+        /*
+        int x = 0;
+        while(x < 210){
+            Robot.MoveForward();
+            x = x+1; 
+        } 
         Robot.TurnRight();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        Robot.MoveForward();
-        
+
+        x = 0;
+        while(x < 100){
+            Robot.MoveForward();
+            x = x+1; 
+        } 
+        x = 0;
+        while(x < 210){
+            Robot.MoveForward();
+            x = x+1; 
+        } 
+        Robot.TurnRight();
+        x = 0;
+        while(x < 100){
+            Robot.MoveForward();
+            x = x+1; 
+        } 
+        */
+       
     }
 }
-
