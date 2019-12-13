d8e5e58febf83b7de33831ecc19c13137e368a51
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-12-13 10:04:48 -0800

    4

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,13 +4,35 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
     public static void Auto() {
+        MoveForward(26);
+            Robot.TurnRight();
+    }   MoveForward(35);
+            Robot.TurnRight();
+        MoveForward(31);
 
-        
+    public static void MoveForward(int y){
+          
+        int x = 0;
+        while( x < y){
+            Robot .MoveForward();
+            x = x + 1;
+        }
+
     }
 }
 
 
-       
+       /*
+    int x = 0; 
+    while( x < 10 ){
+        Robot.MoveForward();
+        x = x + 1; 
+    }
+
+
+
+
+       */
 
     
 
