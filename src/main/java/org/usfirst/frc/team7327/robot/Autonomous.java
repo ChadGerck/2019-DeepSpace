f417cb037dc9cb8d294e4527757d8e5b48dc2cab
Aaron Tuelles <tuelles@icloud.com>
2019-12-17 15:28:45 -0800

    ducky

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -1,17 +1,46 @@
 package org.usfirst.frc.team7327.robot;
 
 import org.usfirst.frc.team7327.robot.Robot; 
-import org.usfirst.frc.team7327.robot.Group6;
 
 public class Autonomous {
 
-    
-    public static Group6 call; 
-
-    public static void Auto() {
-        call.DriveRectangle();
-       
+    public static void Auto(){
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.TurnRight();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.TurnRight();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
+    Robot.MoveForward();
     }
-
 }
-
