a473d3c825bc9af56590dda861a8d442bf17068d
JasonMiller1023 <58994240+JasonMiller1023@users.noreply.github.com>
2019-12-18 11:42:42 -0800

    Update Autonomous.java

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,20 +4,8 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
     public static void Auto() {
-
-        int x = 0;
-        while(x < 10){
-            Robot.MoveForward();
-            x++; 
-        }
-
-        MoveFoward(3);
-
-
-    }
-    public static void MoveFoward(int y){
-
-
+        Robot.MoveForward(); 
+        Robot.TurnRight(); 
     }
 }
 
