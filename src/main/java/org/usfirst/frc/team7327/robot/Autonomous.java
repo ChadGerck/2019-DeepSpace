cb0deb109132fc970e2822517f60fd810dfbbb02
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2019-12-19 10:51:50 -0800

    9

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -8,12 +8,14 @@ public class Autonomous {
         NewLane();
                                                              
     }
-    public static void Leftyturn(int z){
-        int x = 0; 
-        while(x < z ){
-            //Robot.TurnLeft();
+    public static void Leftyturn(){
+     
+       
+            Robot.TurnRight();
+            Robot.TurnRight();
+            Robot.TurnRight();
 
-        }
+        
     }
 
 
